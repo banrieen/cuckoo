c47982c25110b833e7dbc23b8b5eac6c217e1085
Banrieen <banrieen@163.com>
2019-12-30 17:31:00 +0800

    Resolve the django conf

--- ai/models.py
+++ ai/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
-# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        """ 型增加 __str__() 方法是很重要的，这不仅仅能给你在命令行里使用带来方便，
+        Django 自动生成的 admin 里也使用这个方法来表示对象。 """
+        return self.question_text
+    
+
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently ?'
--- ai/views.py
+++ ai/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+
+from . models import Choice, Question
+
+class IndexView(generic.ListView):
+    template_name = 'blog/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """ 返回最新的5个问题 """
+        return Question.objects.filter(
+            pub_date__lte = timezone.now()
+        ).order_by('-pub_date')[:5]
+        # Question.objects.order_by('-pub_date')[:5]
